d94bf8490dfeb83b349a279a40b81a6b203e06bb
cocosharms14 <sharmanthomas.james@gmail.com>
2025-05-18 09:30:25 +0100

    resize nav menue function

---
+++ javascript.js
@@ -0,0 +1,42 @@
+//create a light mode and dark mode function
+//create a responsive navigation bar
+//do it all using 
+
+const responsiveMenu = function() {
+    const waffle = document.querySelector('.menuwaffle')
+    const nav = document.querySelector('.nav')
+    const mobileScreenSize = 500
+    const loadWaffle =  function() {
+        if (window.innerWidth <= mobileScreenSize) {
+        console.log('waffle ON function running...', window.innerWidth)
+         waffle.classList.remove('menuwaffle')
+        nav.classList.remove('nav')
+        waffle.classList.add('waffleOn')
+        nav.classList.add('navOff')
+        }
+        else {
+        console.log('waffle OFF function running...', window.innerWidth)
+        waffle.classList.remove('waffleOn')
+        nav.classList.remove('navOff')
+        waffle.classList.add('menuwaffle')
+        nav.classList.add('nav')
+        }
+
+    }
+    window.addEventListener('resize', () => {loadWaffle(), console.log('resizing...')})
+
+    return {
+        loadWaffle
+    }
+}
+window.onload = responsiveMenu
+
+
+const styleMode = function () {
+console.log('styleMode loaded')
+}
+
+
+
+
+
