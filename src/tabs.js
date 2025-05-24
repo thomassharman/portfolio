c4c82874ff790f0ac6cefeb1460172eb9de816c2
cocosharms14 <sharmanthomas.james@gmail.com>
2025-05-24 15:02:31 +0100

    colours switch based on the active tab

--- src/tabs.js
+++ src/tabs.js
@@ -1,7 +1,10 @@
 const tabFunctions = function() {
+let tab1;
+let tab2;    
+
     const tabs = function () {
-    const tab1 = document.querySelector('#tab-button-1')
-    const tab2 = document.querySelector('#tab-button-2')
+    tab1 = document.querySelector('#tab-button-1')
+    tab2 = document.querySelector('#tab-button-2')
         if (tab1 && tab2) {
             console.log('tabs loaded from tabFunctions successfully. Now add event listeners...')
             tab1.addEventListener('click', clickTab)
@@ -13,14 +16,19 @@ const tabContent = function () {
     }
     const clickTab = function () {
     console.log(this)
-    if (this.textContent === 'About') {
-        console.log('User clicked ',this.textContent,' section...'),
+    const activeTab = this
+    if (activeTab.textContent === 'About') {
+        console.log('User clicked ',activeTab.textContent,' section...');
+        const inactiveTab = tab2
         appendTabContent.firstTab()
+        tabStyles.switch(activeTab, inactiveTab)
         //add a class that changes the background colour of the tab clicked
     }
-    else if (this.textContent === 'Experience') {
-      console.log('User clicked ',this.textContent,' section...'),
+    else if (activeTab.textContent === 'Experience') {
+      console.log('User clicked ',activeTab.textContent,' section...');
+      const inactiveTab = tab1
       appendTabContent.secondTab()
+      tabStyles.switch(activeTab, inactiveTab)
     }
     }
 
@@ -34,12 +42,24 @@ const tabContent = function () {
         }
     }
 
-    return {
-        tabs: tabs(),
-        tabContent: tabContent(),
-        clickTab: clickTab,
+    const tabStyles = {
+        switch: function(thisTab, otherTab) {
+            if (thisTab.classList.contains('tab-button')) {
+            console.log('this tab must have an active class')
+            thisTab.classList.remove('tab-button') 
+            thisTab.classList.add('activeTab') 
+            otherTab.classList.add('tab-button')
+            otherTab.classList.remove('activeTab') 
+            }
+            }
+        }
+        return {
+            tabs: tabs(),
+            tabContent: tabContent(),
+            clickTab: clickTab,
+    }
     }
 
-}
+
 
 export {tabFunctions}
